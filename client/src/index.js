e46d88d06c52d9604682d7922b7ea8d403aae589
Djameldevgit <developpementwebdjamel@gmail.com>
2025-02-08 15:33:08 +0000

    nbv

--- client/src/index.js
+++ client/src/index.js
@@ -2,23 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/global.css';
 import App from './App';
-import DataProvider from './redux/store'
+import DataProvider from './redux/store';
 import reportWebVitals from './reportWebVitals';
 import { I18nextProvider } from 'react-i18next';
 import i18n from './i18n';
+
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
-  <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
-  </React.StrictMode>,
+    <React.StrictMode>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </React.StrictMode>
   </I18nextProvider>,
   document.getElementById('root')
- 
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// ✅ REGISTRAR EL SERVICE WORKER
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js") // Asegúrate de que el path es correcto
+      .then((registration) => {
+        console.log("✅ Service Worker registrado con éxito:", registration);
+      })
+      .catch((error) => {
+        console.error("❌ Error al registrar Service Worker:", error);
+      });
+  });
+}
+
+// Reporte de métricas (opcional)
 reportWebVitals();
